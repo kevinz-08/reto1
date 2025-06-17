fe279f723839f51a0b0a090e4d4f515add80116f
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-06-17 08:22:31 -0500

    feat: :art: ejercicio uno corregido

--- exercise.sql
+++ exercise.sql
@@ -1,2 +1,2 @@
 INSERT into employees (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id) VALUES
-(4, 'kevin', 'gutierrez', 'kevin@gmail.com', '555-666', '2025-16-17', 4, 8000, 1, 40);
+(4, 'Kevin', 'Gutierrez', 'kevin@example.com', '555-9512', '2019-04-10', 2, 95000, 1, 20);
