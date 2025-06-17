88244732a377542983a9fb124f01b96cc6778dae
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-06-17 08:27:50 -0500

    feat: :art: solucion ejercicio 2

--- exercise.sql
+++ exercise.sql
@@ -1,2 +1,7 @@
 INSERT into employees (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id) VALUES
 (4, 'Kevin', 'Gutierrez', 'kevin@example.com', '555-9512', '2019-04-10', 2, 95000, 1, 20);
+
+SELECT *
+FROM dependents
+WHERE employee_id = 1;
+
